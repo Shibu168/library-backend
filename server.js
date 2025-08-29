69a28e0d83a2cf063ca4ce8bd2a37434e3ea9053
shibu-dev-24 <shibukumar@identifyyou.in>
2025-08-29 21:42:25 +0530

    Add front end url of deployement

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://library-frontend-0kki.onrender.com'],
   credentials: true
 };
 app.use(cors(corsOptions));
